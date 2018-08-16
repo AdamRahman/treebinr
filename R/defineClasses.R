1b0172dd6b78a4ef01854d103c2978a23d1abfb5
R. Wayne Oldford <rwoldford@uwaterloo.ca>
2018-08-16 14:53:17 -0400

    Added documentation.

--- R/defineClasses.R
+++ R/defineClasses.R
@@ -1,16 +1,25 @@
 #' The S4 class treebinr to represent the data reduction output from tree binning.
 #' 
-#' @slot points A matrix whose rows represent the points after data reduction.  These
-#'       are the locations of the bins in the original space.
-#' @slot counts These are the counts in the bin, or the number of original values that
-#'       each point now represents
-#' @slot bins A vector of bins, one for each of the reduced points.  Each bin itself
-#'       an instance of the treebinr class `bin` and so contains much more information
-#' @slot tree A large (n-1) by (n-1) matrix containing information necessary to reconstruct
-#'       the tree
+#' @slot points An m by d matrix whose rows represent the points after data reduction.
+#' These are the representative locations for each bin.
+#' @slot counts A vector of length m containing the number of points from 
+#' the original conguration in each bin.
+#' @slot bins Information about each bin in the conguration, returned as an m-element list 
+#' containing objects of class bin.
+#' @slot tree A directed graph adjacency matrix representing the 
+#' binning tree underlying the algorithm.
 #' 
 #' @export treebinr
 treebinr <- setClass("treebinr", slots = c("points", "counts", "bins", "tree"))
 
+#' The S4 class bin to represent bin produced by tree binning.
+#' 
+#' @slot boundary The defined boundary of the bin. The only requirement here is that this 
+#' slot is compatible with the user defined function to test the inclusion of out-of-sample points.
+#' @slot contents A matrix whose rows are the coordinates of the original data points in this bin
+#' @slot measure The bin score associated with this bin
+#' @slot index An internal tracker indicating where the bin is located in the binning tree.
+#' @slot info Any additional information that should be associated with the bin.
+#' 
 #' @export bin
 bin <- setClass("bin", slots = c("boundary", "contents", "measure", "index", "info"))
